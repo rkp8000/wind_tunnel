9893b3100405232f7b9f024c4e4f455b572ab993
rkp8000 <rpang.contact@gmail.com>
2015-08-04 10:24:41 -0700

    Add kinematics module and unit tests.

---
+++ kinematics.py
@@ -0,0 +1,110 @@
+"""
+Functions for calculating kinematic quantities of flight trajectories.
+"""
+from __future__ import print_function, division
+import numpy as np
+np.seterr(all='ignore')
+
+
+def acceleration(velocities, dt):
+    """
+    Calculate acceleration from velocity using central differences.
+
+    :param velocities: 2D array of velocities (rows are timepoints)
+    :param dt: interval between timesteps
+    :return: array of accelerations
+    """
+
+    return np.transpose([np.gradient(velocities[:, dim], dt) for dim in range(velocities.shape[1])])
+
+
+def heading(velocities):
+    """
+    Calculate heading in xy and xz plane, as well as 3d heading.
+
+    :param velocities: 2D array of velocities (rows are timepoints)
+    :return: array of headings (first col xy, second col xz, third col xyz)
+    """
+
+    v_xy = velocities[:, [0, 1]].copy().astype(float)
+    v_xz = velocities[:, [0, 2]].copy().astype(float)
+    v_xyz = velocities.copy().astype(float)
+
+    # normalize each set of velocities
+    norm_xy = np.linalg.norm(v_xy, axis=1)
+    norm_xz = np.linalg.norm(v_xz, axis=1)
+    norm_xyz = np.linalg.norm(v_xyz, axis=1)
+
+    v_xy /= np.tile(norm_xy, (2, 1)).T
+    v_xz /= np.tile(norm_xz, (2, 1)).T
+    v_xyz /= np.tile(norm_xyz, (3, 1)).T
+
+    # array of upwind vectors
+    uw_vec = np.transpose([-np.ones((len(velocities),), dtype=float),
+                           np.zeros((len(velocities),), dtype=float),
+                           np.zeros((len(velocities),), dtype=float)])
+
+    heading_xy = np.arccos((uw_vec[:, [0, 1]] * v_xy).sum(axis=1))
+    heading_xz = np.arccos((uw_vec[:, [0, 2]] * v_xz).sum(axis=1))
+    heading_xyz = np.arccos((uw_vec * v_xyz).sum(axis=1))
+
+    heading_xy[norm_xy == 0] = 0
+    heading_xz[norm_xz == 0] = 0
+    heading_xyz[norm_xyz == 0] = 0
+
+    return np.transpose([heading_xy, heading_xz, heading_xyz]) * 180 / np.pi
+
+
+def angular_velocity(velocities, dt):
+    """
+    Calculate angular velocities.
+
+    :param velocities: 2D array of velocities (rows are timepoints)
+    :param dt: interval between timesteps
+    :return: array of angular velocities
+    """
+
+    # calculate normalized velocity vector
+    v_norm = velocities / np.tile(np.linalg.norm(velocities.astype(float), axis=1), (3, 1)).T
+
+    # get angle between each consecutive pair of normalized velocity vectors
+    d_theta = np.arccos((v_norm[:-1, :] * v_norm[1:, :]).sum(1))
+    a_vel_mag = d_theta / dt
+    # calculate the direction of angular change by computing the cross-
+    # product between each consecutive pair of normalized velocity vectors
+    cp = np.cross(v_norm[:-1, :], v_norm[1:, :])
+    # normalize the cross product array
+    cp /= np.tile(np.linalg.norm(cp.astype(float), axis=1), (3, 1)).T
+    # create angular velocity array and set to zero the places where the magnitude is zero
+    a_vel = cp * np.tile(a_vel_mag, (3, 1)).T
+    a_vel[a_vel_mag == 0] = 0
+    # correct size so that it matches the size of the velocity array
+    a_vel_full = np.zeros((a_vel.shape[0] + 1, a_vel.shape[1]), dtype=float)
+    a_vel_full[:-1] += a_vel
+    a_vel_full[1:] += a_vel
+    a_vel_full[1:-1] /= 2.
+
+    return a_vel_full
+
+
+def distance_from_wall(positions, wall_bounds):
+    """
+    Calculate distance from nearest wall.
+
+    :param positions: 2D array of positions (rows are timepoints)
+    :param wall_bounds: wall boundaries ((x_lower, x_upper), (y_lower, ...), ...)
+    :return: 1D array of distances from wall
+    """
+
+    above_x = positions[:, 0] - wall_bounds[0][0]
+    below_x = wall_bounds[0][1] - positions[:, 0]
+    above_y = positions[:, 1] - wall_bounds[1][0]
+    below_y = wall_bounds[1][1] - positions[:, 1]
+    above_z = positions[:, 2] - wall_bounds[2][0]
+    below_z = wall_bounds[2][1] - positions[:, 2]
+
+    dist_all_walls = np.array([above_x, below_x,
+                               above_y, below_y,
+                               above_z, below_z])
+
+    return np.min(dist_all_walls, axis=0)
---
+++ test_kinematics.py
@@ -0,0 +1,190 @@
+"""
+Unit tests for kinematics calculations.
+"""
+from __future__ import print_function, division
+import unittest
+import numpy as np
+import kinematics
+
+
+class TruismsTestCase(unittest.TestCase):
+
+    def setUp(self):
+        print("In test '{}'...".format(self._testMethodName))
+
+    def test_false_is_true(self):
+        # this should fail
+        self.assertTrue(False)
+
+    def test_true_is_true(self):
+        self.assertTrue(True)
+
+
+class AccelerationTestCase(unittest.TestCase):
+
+    def setUp(self):
+        print("In test '{}'...".format(self._testMethodName))
+
+    def test_example_velocities_are_correctly_differentiated(self):
+
+        v = np.array([[1., 1, 1], [2, 2, 2], [3, 3, 3], [5, 5, 5], [7, 7, 7], [10, 10, 10]])
+        dt = 0.1
+        a_correct = np.array([[1., 1, 1], [1, 1, 1], [1.5, 1.5, 1.5], [2, 2, 2], [2.5, 2.5, 2.5], [3, 3, 3]]) / dt
+
+        np.testing.assert_array_almost_equal(kinematics.acceleration(v, dt), a_correct)
+
+
+class HeadingTestCase(unittest.TestCase):
+
+    def setUp(self):
+        print("In test '{}'...".format(self._testMethodName))
+
+    def test_example_headings_correctly_calculated(self):
+
+        # flight along the axes
+        v = np.array([[-1., 0, 0], [1, 0, 0], [0, 1, 0], [0, -1, 0], [0, 0, 1], [0, 0, -1]])
+        h_correct = np.array([[0, 0, 0],
+                              [180, 180, 180],
+                              [90, 0, 90],
+                              [90, 0, 90],
+                              [0, 90, 90],
+                              [0, 90, 90]])
+        self.assertFalse(np.any(np.isnan(kinematics.heading(v))))
+        np.testing.assert_array_almost_equal(kinematics.heading(v), h_correct)
+
+        v = np.array([[-1, 1, 0], [-1, -1, 0], [-1, 0, 1], [-1, 0, -1], [-1, 1, 1], [-1, -1, -1]])
+        h_correct = np.array([[45, 0, 45],
+                              [45, 0, 45],
+                              [0, 45, 45],
+                              [0, 45, 45],
+                              [45, 45, 54.735610317245346],
+                              [45, 45, 54.735610317245346]])
+        self.assertFalse(np.any(np.isnan(kinematics.heading(v))))
+        np.testing.assert_array_almost_equal(kinematics.heading(v), h_correct)
+
+        v = np.array([[1, -1, 0], [1, 0, 1], [1, 1, 1]])
+        h_correct = np.array([[135, 180, 135],
+                              [180, 135, 135],
+                              [135, 135, 125.26438968275465]])
+        self.assertFalse(np.any(np.isnan(kinematics.heading(v))))
+        np.testing.assert_array_almost_equal(kinematics.heading(v), h_correct)
+
+    def test_all_headings_between_zero_and_180(self):
+
+        # make random velocity vector
+        v = np.random.normal(0, 10, (1000, 3))
+        h = kinematics.heading(v)
+        self.assertFalse(np.any(np.isnan(h)))
+        np.testing.assert_array_less(-.00001 * np.ones(h.shape), h)
+        np.testing.assert_array_less(h, 180.00001 * np.ones(h.shape))
+
+
+class AngularVelocityTestCase(unittest.TestCase):
+
+    def setUp(self):
+        print("In test '{}'...".format(self._testMethodName))
+
+    def test_angular_velocity_higher_during_turns(self):
+
+        dt = 0.1
+
+        # turn around in xy plane
+        v = np.array([[-1., 0, 0], [-1, 0, 0], [-1, 0, 0],
+                      [-1, 1, 0], [0, 1, 0], [1, 1, 0],
+                      [1, 0, 0], [1, 0, 0], [1, 0, 0]])
+        av = kinematics.angular_velocity(v, dt)
+
+        self.assertFalse(np.any(np.isnan(av)))
+        # make sure angular velocity in x-direction is zero
+        np.testing.assert_array_almost_equal(av[:, 0], np.zeros(av[:, 0].shape))
+        # make sure angular velocity in y-direction is zero
+        np.testing.assert_array_almost_equal(av[:, 1], np.zeros(av[:, 1].shape))
+        # make sure angular velocity magnitude in z-direction is higher in middle
+        self.assertGreater(np.abs(av[4, 2]), av[0, 2])
+        self.assertGreater(np.abs(av[4, 2]), av[8, 2])
+        # make sure angular velocity in middle is less than zero (right-hand rule)
+        self.assertLess(av[4, 2], 0)
+
+        # turn around in xy plane the other way
+        v = np.array([[-1., 0, 0], [-1, 0, 0], [-1, 0, 0],
+                      [-1, -1, 0], [0, -1, 0], [1, -1, 0],
+                      [1, 0, 0], [1, 0, 0], [1, 0, 0]])
+        av = kinematics.angular_velocity(v, dt)
+
+        self.assertFalse(np.any(np.isnan(av)))
+        # make sure angular velocity in x-direction is zero
+        np.testing.assert_array_almost_equal(av[:, 0], np.zeros(av[:, 0].shape))
+        # make sure angular velocity in y-direction is zero
+        np.testing.assert_array_almost_equal(av[:, 1], np.zeros(av[:, 1].shape))
+        # make sure angular velocity in z-direction is higher in middle
+        self.assertGreater(np.abs(av[4, 2]), av[0, 2])
+        self.assertGreater(np.abs(av[4, 2]), av[8, 2])
+        # make sure angular velocity in middle is greater than zero (right-hand rule)
+        self.assertGreater(av[4, 2], 0)
+
+        # turn around in xz plane
+        v = np.array([[-1., 0, 0], [-1, 0, 0], [-1, 0, 0],
+                      [-1, 0, 1], [0, 0, 1], [1, 0, 1],
+                      [1, 0, 0], [1, 0, 0], [1, 0, 0]])
+        av = kinematics.angular_velocity(v, dt)
+
+        self.assertFalse(np.any(np.isnan(av)))
+        # make sure angular velocity in x-direction is zero
+        np.testing.assert_array_almost_equal(av[:, 0], np.zeros(av[:, 0].shape))
+        # make sure angular velocity in z-direction is zero
+        np.testing.assert_array_almost_equal(av[:, 2], np.zeros(av[:, 2].shape))
+        # make sure angular velocity in y-direction is higher in middle
+        self.assertGreater(np.abs(av[4, 1]), av[0, 1])
+        self.assertGreater(np.abs(av[4, 1]), av[8, 1])
+        # make sure angular velocity in middle is greater than zero (right-hand rule)
+        self.assertGreater(av[4, 1], 0)
+
+        # turn around in xz plane the other way
+        v = np.array([[-1., 0, 0], [-1, 0, 0], [-1, 0, 0],
+                      [-1, 0, -1], [0, 0, -1], [1, 0, -1],
+                      [1, 0, 0], [1, 0, 0], [1, 0, 0]])
+        av = kinematics.angular_velocity(v, dt)
+
+        self.assertFalse(np.any(np.isnan(av)))
+        # make sure angular velocity in x-direction is zero
+        np.testing.assert_array_almost_equal(av[:, 0], np.zeros(av[:, 0].shape))
+        # make sure angular velocity in z-direction is zero
+        np.testing.assert_array_almost_equal(av[:, 2], np.zeros(av[:, 2].shape))
+        # make sure angular velocity in y-direction is higher in middle
+        self.assertGreater(np.abs(av[4, 1]), av[0, 1])
+        self.assertGreater(np.abs(av[4, 1]), av[8, 1])
+        # make sure angular velocity in middle is less than zero (right-hand rule)
+        self.assertLess(av[4, 1], 0)
+
+
+class DistanceToWallTestCase(unittest.TestCase):
+
+    def setUp(self):
+        print("In test '{}'...".format(self._testMethodName))
+
+    def example_distances_calculated_correctly(self):
+        wall_bounds = ((-2, 4), (-1, 3), (-3, 4))
+
+        positions = np.array([[-1., 1, 1],
+                              [-2., 1, 1],
+                              [-2., 0.5, 1],
+                              [-3., 0.25, 1],
+                              [-3, -0.5, 1]])
+
+        d_correct = np.array([1, 0, 0, -1, -1])
+        d = kinematics.distance_from_wall(positions, wall_bounds)
+        np.testing.assert_array_almost_equal(d, d_correct)
+
+        positions = np.array([[0., 0, 0],
+                              [1, 0, 5],
+                              [1, 0, 0],
+                              [-2, 0, 0],
+                              [-2, 0, -3]])
+
+        d_correct = np.array([1, -1, 1, -1, -1])
+        d = kinematics.distance_from_wall(positions, wall_bounds)
+        np.testing.assert_array_almost_equal(d, d_correct)
+
+
+if __name__ == '__main__':
+    unittest.main()
