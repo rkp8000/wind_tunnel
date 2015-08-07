296a8ec2a0b07a4892e7015621bb7b0c7f696620
rkp8000 <rpang.contact@gmail.com>
2015-08-07 11:50:30 -0700

    Add cross-covariance function for multiple time-series (+ tests).

--- test_time_series.py
+++ test_time_series.py
@@ -5,6 +5,7 @@ from __future__ import print_function, division
 import unittest
 import numpy as np
 import time_series
+from scipy import ndimage
 
 
 class SegmentingTestCase(unittest.TestCase):
@@ -59,6 +60,56 @@ class SegmentingTestCase(unittest.TestCase):
 
         np.testing.assert_array_equal(starts, starts_correct)
         np.testing.assert_array_equal(ends, ends_correct)
+
+
+class CrossCovarianceTestCase(unittest.TestCase):
+
+    def test_autocovariance_is_delta_function_for_white_noise(self):
+
+        xs = [np.random.normal(0, 3, np.random.randint(500, 1000)) for _ in range(100)]
+        acov, pv, conf_lb, conf_ub = \
+            time_series.xcov_multi_with_confidence(xs, xs, lag_forward=21, lag_backward=20, normed=True)
+
+        self.assertEqual(len(acov), 41)
+        self.assertAlmostEqual(acov[:20].mean(), 0, places=2)
+        self.assertAlmostEqual(acov[21:].mean(), 0, places=2)
+        self.assertAlmostEqual(acov[20], 1, places=5)
+
+        np.testing.assert_array_almost_equal(acov[:20], acov[21:][::-1])
+        np.testing.assert_array_almost_equal(pv[:20], pv[21:][::-1])
+        np.testing.assert_array_almost_equal(conf_lb[:20], conf_lb[21:][::-1])
+        np.testing.assert_array_almost_equal(conf_ub[:20], conf_ub[21:][::-1])
+
+    def test_autocovariance_is_extended_for_smoother_signals_and_has_correct_confidences(self):
+
+        xs = [np.random.normal(0, 1, np.random.randint(500, 1000)) for _ in range(100)]
+        xs_smooth = [ndimage.gaussian_filter1d(x, 5) for x in xs]
+
+        acov, _, _, _ = time_series.\
+            xcov_multi_with_confidence(xs, xs, lag_forward=21, lag_backward=20, normed=True)
+        acov_smooth, _, lb, ub = time_series.\
+            xcov_multi_with_confidence(xs_smooth, xs_smooth, lag_forward=21, lag_backward=20, normed=True)
+
+        for lag in range(2, 11):
+            self.assertLess(acov[20 + lag], acov_smooth[20 + lag])
+            self.assertLess(acov[20 + lag], acov_smooth[20 + lag])
+            self.assertLess(acov[20 - lag], acov_smooth[20 - lag])
+            self.assertLess(acov[20 - lag], acov_smooth[20 - lag])
+
+        np.testing.assert_array_less(lb[:20], ub[:20])
+        self.assertTrue(np.all(lb[:20] > -1))
+        self.assertTrue(np.all(ub[:20] < 1))
+
+    def test_unnormed_autocovariance_gives_variance_at_0_lag(self):
+
+        xs = [np.random.normal(0, 1, np.random.randint(500, 1000)) for _ in range(100)]
+        xs = [ndimage.gaussian_filter1d(x, 5) for x in xs]
+
+        acov, _, _, _ = time_series.\
+            xcov_multi_with_confidence(xs, xs, lag_forward=21, lag_backward=20, normed=False)
+
+        self.assertAlmostEqual(acov[20], np.var(np.concatenate(xs)), places=5)
+
 
 if __name__ == '__main__':
     unittest.main()
--- time_series.py
+++ time_series.py
@@ -4,6 +4,7 @@ Functions for playing with time-serieses.
 from __future__ import print_function, division
 import numpy as np
 from numpy import concatenate as cc
+import stats
 
 
 def segment_basic(x, t=None):
@@ -21,3 +22,72 @@ def segment_basic(x, t=None):
     ends = t[(np.diff(cc([x, [False]]).astype(int)) == -1).nonzero()[0]] + 1
 
     return starts, ends
+
+
+def xcov_multi_with_confidence(xs, ys, lag_forward, lag_backward, confidence=0.95, normed=False):
+    """
+    Calculate cross-covariance between x and y when multiple time-series are available.
+        This function is to be used when it is believed that y is created by filtering x
+        with a causal linear filter. If that is the case, then as the number of samples
+        increases, the result will approach the shape of the original filter.
+    :param xs: list of input time-series
+    :param ys: list of output time-series
+    :param lag_forward: number of lags to look forward (causal (x yields y))
+    :param lag_backward: number of lags to look back (acausal (y yields x))
+    :param confidence: confidence of confidence interval desired
+    :param normed: if True, results will be normalized by geometric mean of x's & y's variances
+    :return: cross-covariance, p-value, lower bound, upper bound
+    """
+
+    if not np.all([len(x) == len(y) for x, y in zip(xs, ys)]):
+        raise ValueError('Arrays within xs and ys must all be of the same size!')
+
+    covs = []
+    p_values = []
+    lbs = []
+    ubs = []
+
+    for lag in range(-lag_backward, lag_forward):
+        if lag == 0:
+            x_rel = xs
+            y_rel = ys
+        elif lag < 0:
+            x_rel = [x[-lag:] for x in xs if len(x) > -lag]
+            y_rel = [y[:lag] for y in ys if len(y) > -lag]
+        else:
+            # calculate the cross covariance between x and y with a specific lag
+            # first get the relevant xs and ys from each time-series
+            x_rel = [x[:-lag] for x in xs if len(x) > lag]
+            y_rel = [y[lag:] for y in ys if len(y) > lag]
+
+        all_xs = np.concatenate(x_rel)
+        all_ys = np.concatenate(y_rel)
+
+        cov = np.cov(all_xs, all_ys)
+        covs += [cov[0, 1]]
+
+        rho, p_value, lb, ub = stats.pearsonr_with_confidence(all_xs, all_ys, confidence)
+
+        p_values += [p_value]
+        lbs += [lb]
+        ubs += [ub]
+
+        if lag == 0:
+            var_x, var_y = cov[0, 0], cov[1, 1]
+
+    covs = np.array(covs)
+    p_values = np.array(p_values)
+    lbs = np.array(lbs)
+    ubs = np.array(ubs)
+
+    norm_factor = np.sqrt(var_x * var_y)
+
+    if normed:
+        # normalize by mean variance of signals
+        covs /= norm_factor
+    else:
+        # convert confidence bounds to covariances
+        lbs *= norm_factor
+        ubs *= norm_factor
+
+    return covs, p_values, lbs, ubs
