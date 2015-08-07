9c3f2e07bc276bb96d335b04f33450f52dd837fc
rkp8000 <rpang.contact@gmail.com>
2015-08-07 10:56:38 -0700

    Make this module also determine the figure data environment variable in accordance with whether we are in test mode or not.

--- db_api/connect.py
+++ db_api/connect.py
@@ -7,4 +7,33 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 
-TEST = False
+TEST = True
+COMMIT = False
+
+
+def commit(sesh):
+    if COMMIT:
+        sesh.commit()
+
+
+if TEST:
+    print('CONNECTED TO WIND TUNNEL TEST DATABASE')
+
+    engine = create_engine(os.environ['WIND_TUNNEL_TEST_DB_CXN_URL'])
+    figure_data_env_var = 'WIND_TUNNEL_TEST_FIGURE_DATA_DIRECTORY'
+
+else:
+    print('CONNECTED TO WIND TUNNEL PRODUCTION DATABASE')
+
+    x = raw_input('Are you sure you want to connect to the production database [y or n]?')
+    if x.lower() == 'y':
+        engine = create_engine(os.environ['WIND_TUNNEL_DB_CXN_URL'])
+    else:
+        raise RuntimeError('User prevented write access to database.')
+
+    figure_data_env_var = 'WIND_TUNNEL_FIGURE_DATA_DIRECTORY'
+
+engine.connect()
+
+Session = sessionmaker(bind=engine)
+session = Session()
