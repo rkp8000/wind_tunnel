e9b5c800247094f58d4ddf0b2ea8c895f9b92976
rkp8000 <rpang.contact@gmail.com>
2015-10-07 12:04:16 -0700

    Added function to orthogonalize stored basis and added 'start' parameter to munge function to specify starting timestep.

--- test_time_series.py
+++ test_time_series.py
@@ -290,6 +290,116 @@ class TimeSeriesMungerTestCase(unittest.TestCase):
             y[80:90][None, :].dot(sin_basis_short).flatten()
         )
 
+    def test_feature_matrix_correctly_formed_with_basis_functions_with_nonzero_start(self):
+        # three features, 1 output
+        x1 = np.random.normal(0, 10, (100,))
+        x2 = np.random.normal(0, 10, (100,))
+        x3 = np.random.normal(0, 10, (100,))
+        y = np.random.normal(0, 10, (100,))
+
+        tsm = time_series.Munger()
+        tsm.delay = 10  # we will assume there is a 10 timestep delay between input and response
+
+        # we will assume x1 is used directly and x2, x3, and y are filtered, and that their
+        # filters are represented by sums of sinusoidal basis functions
+        # (the details don't matter as we're just making sure all the arrays are getting
+        # rearranged correctly)
+        t = np.linspace(0, np.pi, 20)
+        sin_basis = np.transpose([np.sin(t), np.sin(2*t), np.sin(3*t), np.sin(4*t)])
+        t_short = t[:10]
+        sin_basis_short = np.transpose([np.sin(t_short), np.sin(2*t_short)])
+        tsm.basis_in = [None, sin_basis, sin_basis]
+        tsm.basis_out = sin_basis_short
+
+        feature_matrix, response_vector = tsm.munge([x1, x2, x3], y, start=5)
+
+        # make sure the arrays are the correct shape
+        self.assertEqual(len(response_vector), 85)
+        self.assertEqual(feature_matrix.shape[0], 85)
+        self.assertEqual(feature_matrix.shape[1], 12)
+
+        # make sure the constants and x1 terms are in the correct places (check 1st, 2nd, last)
+        self.assertAlmostEqual(feature_matrix[0, 0], 1)
+        self.assertAlmostEqual(feature_matrix[1, 0], 1)
+        self.assertAlmostEqual(feature_matrix[-1, 0], 1)
+        self.assertAlmostEqual(feature_matrix[0, 1], x1[5])
+        self.assertAlmostEqual(feature_matrix[1, 1], x1[6])
+        self.assertAlmostEqual(feature_matrix[-1, 1], x1[89])
+
+        # make sure the rest of the features correspond to the projections of x2, x3, and y
+        # onto their appropriate basis functions
+        np.testing.assert_array_almost_equal(
+            feature_matrix[14, 2:6],
+            x2[:20][None, :].dot(sin_basis).flatten()
+        )
+        np.testing.assert_array_almost_equal(
+            feature_matrix[14, 6:10],
+            x3[:20][None, :].dot(sin_basis).flatten()
+        )
+        np.testing.assert_array_almost_equal(
+            feature_matrix[4, 10:12],
+            y[:10][None, :].dot(sin_basis_short).flatten()
+        )
+
+        np.testing.assert_array_almost_equal(
+            feature_matrix[15, 2:6],
+            x2[1:21][None, :].dot(sin_basis).flatten()
+        )
+        np.testing.assert_array_almost_equal(
+            feature_matrix[15, 6:10],
+            x3[1:21][None, :].dot(sin_basis).flatten()
+        )
+        np.testing.assert_array_almost_equal(
+            feature_matrix[5, 10:12],
+            y[1:11][None, :].dot(sin_basis_short).flatten()
+        )
+
+        np.testing.assert_array_almost_equal(
+            feature_matrix[-1, 2:6],
+            x2[70:90][None, :].dot(sin_basis).flatten()
+        )
+        np.testing.assert_array_almost_equal(
+            feature_matrix[-1, 6:10],
+            x3[70:90][None, :].dot(sin_basis).flatten()
+        )
+        np.testing.assert_array_almost_equal(
+            feature_matrix[-1, 10:12],
+            y[80:90][None, :].dot(sin_basis_short).flatten()
+        )
+
+    def test_orthogonalization_of_nonorthogonal_basis_functions(self):
+
+        non_orth_basis_1 = np.array([[0., 1], [1, 1], [0, 0]])
+        non_orth_basis_2 = np.random.normal(0, 1, (20, 3))
+        non_orth_basis_3 = np.random.uniform(0, 1, (20, 5))
+
+        tsm = time_series.Munger()
+        tsm.delay = 4
+        tsm.basis_in = [None, non_orth_basis_1, non_orth_basis_2]
+        tsm.basis_out = non_orth_basis_3
+
+        tsm.orthogonalize_basis()
+
+        self.assertTrue(tsm.basis_in[0] is None)
+
+        # check orthogonality of basis_in[1]
+        self.assertAlmostEqual(tsm.basis_in[1][:, 0].dot(tsm.basis_in[1][:, 1]), 0)
+        # check orthogonality of basis_in[2]
+        self.assertAlmostEqual(tsm.basis_in[2][:, 0].dot(tsm.basis_in[2][:, 1]), 0)
+        self.assertAlmostEqual(tsm.basis_in[2][:, 0].dot(tsm.basis_in[2][:, 2]), 0)
+        self.assertAlmostEqual(tsm.basis_in[2][:, 1].dot(tsm.basis_in[2][:, 2]), 0)
+        # check orthogonality of basis_out
+        self.assertAlmostEqual(tsm.basis_out[:, 0].dot(tsm.basis_out[:, 1]), 0)
+        self.assertAlmostEqual(tsm.basis_out[:, 0].dot(tsm.basis_out[:, 2]), 0)
+        self.assertAlmostEqual(tsm.basis_out[:, 0].dot(tsm.basis_out[:, 3]), 0)
+        self.assertAlmostEqual(tsm.basis_out[:, 0].dot(tsm.basis_out[:, 4]), 0)
+        self.assertAlmostEqual(tsm.basis_out[:, 1].dot(tsm.basis_out[:, 2]), 0)
+        self.assertAlmostEqual(tsm.basis_out[:, 1].dot(tsm.basis_out[:, 3]), 0)
+        self.assertAlmostEqual(tsm.basis_out[:, 1].dot(tsm.basis_out[:, 4]), 0)
+        self.assertAlmostEqual(tsm.basis_out[:, 2].dot(tsm.basis_out[:, 3]), 0)
+        self.assertAlmostEqual(tsm.basis_out[:, 2].dot(tsm.basis_out[:, 4]), 0)
+        self.assertAlmostEqual(tsm.basis_out[:, 3].dot(tsm.basis_out[:, 4]), 0)
+
 
 if __name__ == '__main__':
     unittest.main()
