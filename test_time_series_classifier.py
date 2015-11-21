6719bf9f2af46e62701629ff884fbcbd690863f5
Rich Pang <rpang.contact@gmail.com>
2015-11-20 20:23:03 -0800

    Add calculation of multivariate normal log probability of set of vectors given means and covariance, and test it.

--- test_time_series_classifier.py
+++ test_time_series_classifier.py
@@ -3,11 +3,31 @@ Unit tests for time-series classifiers.
 """
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy import stats
+import random
 import unittest
 
 import time_series_classifier as tsc
 
 cc = np.concatenate
+
+
+class VARStaticMethodsTestCase(unittest.TestCase):
+
+    def test_log_prob_is_calculated_correctly(self):
+        data = np.random.normal(0, 1.4, (4, 10))
+        means = np.random.normal(0, 1, (4, 10))
+        cov = np.cov(np.random.uniform(-1, 1, (4, 100)))
+
+        # estimation using for-loop and python's mvn pdf
+        logpdf_true = np.array(
+            [stats.multivariate_normal.logpdf(data[:, col], means[:, col], cov)
+             for col in range(data.shape[1])]
+        )
+        # estimate using var model instance
+        logpdf_var = tsc.VarModel.log_prob_mvn(data, means, cov)
+
+        np.testing.assert_array_almost_equal(logpdf_true, logpdf_var)
 
 
 class VARTestCase(unittest.TestCase):
@@ -43,9 +63,15 @@ class VARTestCase(unittest.TestCase):
 
     def test_we_can_classify_time_series_generated_from_ideal_distributions(self):
 
+        # make 2 models
         var_model_1 = tsc.VarModel(a=self.a_1, k=self.k_1)
         var_model_2 = tsc.VarModel(a=self.a_2, k=self.k_2)
 
+        # check that full matrix has correct shape
+        self.assertEqual(var_model_1.a_full.shape, (3, 6))
+        self.assertEqual(var_model_2.a_full.shape, (3, 6))
+
+        # sample some time-series from the models
         tss_1 = [var_model_1.sample(initial='zero', t=500) for _ in range(20)]
         tss_2 = [var_model_2.sample(initial='zero', t=500) for _ in range(20)]
 
@@ -61,13 +87,13 @@ class VARTestCase(unittest.TestCase):
 
         # make sure that probability of time-series are highest under the models that made them
         for _ in range(10):
-            ts_1 = np.random.choice(tss_1)
-            ts_2 = np.random.choice(tss_2)
+            ts_1 = random.choice(tss_1)
+            ts_2 = random.choice(tss_2)
 
             self.assertGreater(var_model_1.log_prob(ts_1), var_model_2.log_prob(ts_1))
             self.assertGreater(var_model_2.log_prob(ts_2), var_model_1.log_prob(ts_2))
 
-        # make sure we can classify time-series correctly
+        # make sure we can classify time-series correctly using the classifier
         f = tsc.VarClassifierBinary()
         f.train(positives=tss_1, negatives=tss_2)
         predictions = f.predict(tss_2 + tss_1)
--- time_series_classifier.py
+++ time_series_classifier.py
@@ -10,9 +10,14 @@ class VarModel(object):
     def __init__(self, a, k):
         self.a = a
         self.k = k
+        self.k_inv = np.linalg.inv(k)
+        self.k_det = np.linalg.det(k)
         self.noise_mean = np.zeros((len(k),), dtype=float)
         self.order = len(a)
         self.dim = len(k)
+
+        # create full a matrix so that entire update can be done in one multiplication
+        self.a_full = np.concatenate(a, axis=1)
 
     def sample(self, t, initial='zero'):
         """
@@ -21,18 +26,21 @@ class VarModel(object):
         :param initial: how to set the initial values
         :return: 2D numpy array, rows are time-points
         """
+
         ts = np.nan * np.zeros((t, self.dim), dtype=float)
+
         if initial == 'zero':
             ts[:self.order, :] = 0
         else:
             ts[:self.order, :] = initial
 
         for ctr in range(self.order, t):
-            next_tp = np.zeros((self.dim, 1))
-            for d_ctr in range(self.order):
-                next_tp += self.a[d_ctr].dot(ts[ctr - d_ctr, :][:, None])
-            ts[ctr] = next_tp.flatten()
-
+            # get concatenated version of previous timesteps
+            prev_stack = ts[ctr - self.order:ctr, :]
+            prev_cc = np.concatenate(np.flipud(prev_stack))[:, None]
+            mean = self.a_full.dot(prev_cc).flatten()
+            next_tp = np.random.multivariate_normal(mean, self.k)
+            ts[ctr] = next_tp
         return ts
 
     def log_prob(self, tss):
@@ -43,7 +51,40 @@ class VarModel(object):
         :param tss: list of time-series
         :return: log probability
         """
-        pass
+        # calculate predictions at each time point
+        predictors = np.concatenate([self.munge(ts, order=self.order) for ts in tss], axis=0)
+        predictions = self.a_full.dot(predictors.T)
+        truths = np.concatenate([ts[self.order:, :] for ts in tss], axis=0).T
+
+        log_probs = self.log_prob_mvn(truths, means=predictions, cov_inv=self.k_inv, cov_det=self.k_det)
+        return log_probs.sum()
+
+    @staticmethod
+    def log_prob_mvn(data, means, cov=None, cov_inv=None, cov_det=None):
+        """
+        Calculate the log-probabilities of a set of multivariate data points given a set of means and a
+        covariance matrix.
+        :param data: data array - rows are variables, columns are samples
+        :param means: array of means, same size as data
+        :param cov: covariance matrix
+        :param cov_inv: inverse covariance matrix (optional)
+        :param cov_det: covariance matrix determinant (optional)
+        :return: array of log probabilities, one for each data point
+        """
+        if cov_inv is None:
+            cov_inv = np.linalg.inv(cov)
+        if cov_det is None:
+            cov_det = np.linalg.det(cov)
+
+        # calculate difference between data and means
+        diffs = data - means
+        dim = diffs.shape[0]
+        # calculate quadratic term of probability
+        quad_terms = -(diffs * cov_inv.dot(diffs)).sum(axis=0) / 2
+        # calculate normalization factor
+        norm_factor = np.log((2*np.pi) ** (-0.5*dim)) + np.log(cov_det ** -0.5)
+
+        return quad_terms + norm_factor
 
 
 class VarClassifierBinary(object):
