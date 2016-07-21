09fe1e68e8a3aa780a314453339bbd1c151887c6
rkp8000 <rpang.contact@gmail.com>
2016-07-21 13:56:51 -0700

    Start code for simple binary vs. binary-linear model comparison.

---
+++ simple_models.py
@@ -0,0 +1,13 @@
+from __future__ import division, print_function
+import numpy as np
+from sklearn import linear_model
+
+
+class BinaryHeadingConcModel(object):
+
+    pass
+
+
+class BinaryLinearHeadingConcModel(object):
+
+    pass
