9c06d8bc4c8986a446cd4b5cc9e687f23ae15889
rkp8000 <rpang.contact@gmail.com>
2016-07-21 11:07:03 -0700

    Put these in their own importable module.

---
+++ experimental_constants.py
@@ -0,0 +1,38 @@
+PLUME_PARAMS_DICT = {
+    'fruitfly_0.3mps_checkerboard_floor': {
+        'max_conc': 194.4013571501637,
+        'threshold': 10,
+        'ymean': -0.01686711918990497,
+        'zmean': -0.024448968890361408,
+        'ystd': 0.013907665641150927,
+        'zstd': 0.024051865551085332,
+    },
+    'fruitfly_0.4mps_checkerboard_floor': {
+        'max_conc': 526.5611158275608,
+        'threshold': 10,
+        'ymean': 0.010397528868787908,
+        'zmean': 0.021053005635139854,
+        'ystd': 0.011163543244851111,
+        'zstd': 0.012783432241805101,
+    },
+    'fruitfly_0.6mps_checkerboard_floor': {
+        'max_conc': 470.45545552729538,
+        'threshold': 10,
+        'ymean': 0.0046019902473019899,
+        'zmean': 0.051356187443552505,
+        'ystd': 0.0084829247732408405,
+        'zstd': -0.0091643524066678372,
+    },
+    'mosquito_0.4mps_checkerboard_floor': {
+        'Q': -0.26618286981003886,
+        'u': 0.4,
+        'u_star': 0.06745668765535813,
+        'alpha_y': -0.066842568000323691,
+        'alpha_z': 0.14538827993452938,
+        'x_source': -0.64790143304753445,
+        'y_source': .003,
+        'z_source': .011,
+        'bkgd': 400,
+        'threshold': 450,
+    }
+}
