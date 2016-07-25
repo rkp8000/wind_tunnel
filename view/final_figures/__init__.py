a7893e6763017f244b58722999f84c70d7f5060e
rkp8000 <rpang.contact@gmail.com>
2016-07-25 13:07:40 -0700

    Make figure for history dependence of headings.

--- view/final_figures/__init__.py
+++ view/final_figures/__init__.py
@@ -2,6 +2,7 @@ from __future__ import division, print_function
 import matplotlib.pyplot as plt
 import numpy as np
 from pprint import pprint
+from scipy.stats import ks_2samp
 
 from db_api import models
 from db_api.connect import session
@@ -269,16 +270,198 @@ def heading_concentration_dependence(
 
 
 def early_vs_late_heading_timecourse(
-        EXPT_IDS,
+        CROSSING_GROUP_IDS, CROSSING_GROUP_LABELS,
         X_0_MIN, X_0_MAX, H_0_MIN, H_0_MAX,
+        MAX_CROSSINGS_EARLY, SUBTRACT_INITIAL_HEADING,
         T_BEFORE, T_AFTER,
-        FIG_SIZE, EARLY_LATE_COLORS, FONT_SIZE):
+        AX_SIZE, AX_GRID, EARLY_LATE_COLORS, ALPHA,
+        P_VAL_COLOR, P_VAL_Y_LIM, LEGEND_CROSSING_GROUP_ID,
+        X_0_BINS, FONT_SIZE):
     """
     Show early vs. late headings for different experiments, along with a plot of the
     p-values for the difference between the two means.
     """
 
-    pass
+    # convert times to time steps
+
+    ts_before = int(round(T_BEFORE / DT))
+    ts_after = int(round(T_AFTER / DT))
+
+    # loop over crossing groups
+
+    x_0s_dict = {}
+    headings_dict = {}
+    p_vals_dict = {}
+
+    for cg_id in CROSSING_GROUP_IDS:
+
+        # get crossing group
+
+        crossing_group = session.query(models.CrossingGroup).filter_by(id=cg_id).first()
+
+        # get early and late crossings
+
+        crossings_dict = {}
+        crossings_all = session.query(models.Crossing).filter_by(crossing_group=crossing_group)
+        crossings_dict['early'] = crossings_all.filter(models.Crossing.crossing_number <= MAX_CROSSINGS_EARLY)
+        crossings_dict['late'] = crossings_all.filter(models.Crossing.crossing_number > MAX_CROSSINGS_EARLY)
+
+        x_0s_dict[cg_id] = {}
+        headings_dict[cg_id] = {}
+
+        for label in ['early', 'late']:
+
+            x_0s = []
+            headings = []
+
+            # get all initial headings, initial xs, peak concentrations, and heading time-series
+
+            for crossing in crossings_dict[label]:
+
+                # throw away crossings that do not meet trigger criteria
+
+                x_0 = getattr(crossing.feature_set_basic, 'position_x_{}'.format('peak'))
+                h_0 = getattr(crossing.feature_set_basic, 'heading_xyz_{}'.format('peak'))
+
+                if not (X_0_MIN <= x_0 <= X_0_MAX):
+
+                    continue
+
+                if not (H_0_MIN <= h_0 <= H_0_MAX):
+
+                    continue
+
+                # store x_0 (uw/dw position)
+
+                x_0s.append(x_0)
+
+                # get and store headings
+
+                temp = crossing.timepoint_field(
+                    session, 'heading_xyz', -ts_before, ts_after - 1,
+                    'peak', 'peak', nan_pad=True)
+
+                # subtract initial heading if desired
+
+                if SUBTRACT_INITIAL_HEADING:
+
+                    temp -= temp[ts_before]
+
+                # store headings
+
+                headings.append(temp)
+
+            x_0s_dict[cg_id][label] = np.array(x_0s)
+            headings_dict[cg_id][label] = np.array(headings)
+
+        # loop through all time points and calculate p-value (ks-test) between early and late
+
+        p_vals = []
+
+        for t_step in range(ts_before + ts_after):
+
+            early_with_nans = headings_dict[cg_id]['early'][:, t_step]
+            late_with_nans = headings_dict[cg_id]['late'][:, t_step]
+
+            early_no_nans = early_with_nans[~np.isnan(early_with_nans)]
+            late_no_nans = late_with_nans[~np.isnan(late_with_nans)]
+
+            p_vals.append(ks_2samp(early_no_nans, late_no_nans)[1])
+
+        p_vals_dict[cg_id] = p_vals
+
+
+    ## MAKE PLOTS
+
+    t = np.arange(-ts_before, ts_after) * DT
+
+    # history-dependence
+
+    fig_size = (AX_SIZE[0] * AX_GRID[1], AX_SIZE[1] * AX_GRID[0])
+
+    fig_0, axs_0 = plt.subplots(*AX_GRID, figsize=fig_size, tight_layout=True)
+
+    for cg_id, ax in zip(CROSSING_GROUP_IDS, axs_0.flat):
+
+        # get mean and sem of headings for early and late groups
+
+        handles = []
+
+        for label, color in EARLY_LATE_COLORS.items():
+
+            headings_mean = np.nanmean(headings_dict[cg_id][label], axis=0)
+            headings_sem = stats.nansem(headings_dict[cg_id][label], axis=0)
+
+            handles.append(ax.plot(t, headings_mean, color=color, lw=2, label=label, zorder=1)[0])
+            ax.fill_between(
+                t, headings_mean - headings_sem, headings_mean + headings_sem,
+                color=color, alpha=ALPHA, zorder=1)
+
+        ax.set_xlabel('time since odor peak (s)')
+
+        if SUBTRACT_INITIAL_HEADING:
+
+            ax.set_ylabel('$\Delta$ heading (degrees)')
+
+        else:
+
+            ax.set_ylabel('heading (degrees)')
+
+        ax.set_title(CROSSING_GROUP_LABELS[cg_id])
+
+        if cg_id == LEGEND_CROSSING_GROUP_ID:
+
+            ax.legend(handles=handles, loc='lower left')
+
+        set_fontsize(ax, FONT_SIZE)
+
+        # plot p-value
+
+        ax_twin = ax.twinx()
+
+        ax_twin.plot(t, p_vals_dict[cg_id], color=P_VAL_COLOR, lw=2, ls='--', zorder=0)
+
+        ax_twin.axhline(0.05, ls='-', lw=2, color='gray')
+
+        ax_twin.set_ylim(*P_VAL_Y_LIM)
+        ax_twin.set_ylabel('p-value (KS test)', fontsize=FONT_SIZE)
+
+        set_fontsize(ax_twin, FONT_SIZE)
+
+    # position histograms
+
+    bincs = 0.5 * (X_0_BINS[:-1] + X_0_BINS[1:])
+
+    fig_1, axs_1 = plt.subplots(*AX_GRID, figsize=fig_size, tight_layout=True)
+
+    for cg_id, ax in zip(CROSSING_GROUP_IDS, axs_1.flat):
+
+        # create early and late histograms
+
+        handles = []
+
+        for label, color in EARLY_LATE_COLORS.items():
+
+            probs = np.histogram(x_0s_dict[cg_id][label], bins=X_0_BINS, normed=True)[0]
+
+            handles.append(ax.plot(100*bincs, probs, lw=2, color=color, label=label)[0])
+
+        p_val = ks_2samp(x_0s_dict[cg_id]['early'], x_0s_dict[cg_id]['late'])[1]
+        x_0_mean_diff = x_0s_dict[cg_id]['late'].mean() - x_0s_dict[cg_id]['early'].mean()
+
+        ax.set_xlabel(r'$x_0$ (cm)')
+        ax.set_ylabel('proportion of\ncrossings')
+
+        title = '{0} ($\Delta mean(x_0)$ = {1:10.2} cm) \n(p = {2:10.5f} [KS test])'.format(
+            CROSSING_GROUP_LABELS[cg_id], 100 * x_0_mean_diff, p_val)
+
+        ax.set_title(title)
+
+        ax.legend(handles=handles, loc='best')
+
+        set_fontsize(ax, FONT_SIZE)
+
+    return fig_0, fig_1
 
 
 def infotaxis_analysis(
