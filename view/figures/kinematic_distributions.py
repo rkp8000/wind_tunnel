9035e1cd519e6e6b5d95478d502f92309befdae0
rkp8000 <rpang.contact@gmail.com>
2016-09-19 13:27:45 -0700

    Change figure-making function to standard format.

--- view/figures/kinematic_distributions.py
+++ view/figures/kinematic_distributions.py
@@ -2,42 +2,47 @@
 View set of kinematic distributions.
 """
 from __future__ import print_function, division
+from itertools import product as cproduct
 import matplotlib.pyplot as plt
 from db_api.connect import session
 from db_api import models
-from config.kinematic_distributions import FIG_LAYOUTS
-import plot
+from plot import set_font_size
 
 
-FACE_COLOR = 'white'
-FONT_SIZE = 16
-AX_SIZE = (6, 4)
-LW = 2
-COLORS = ('b', 'g', 'r')
+def show_distributions(EXPERIMENTS, VARIABLES, ODOR_STATES, AX_GRID):
 
-for fig_layout in FIG_LAYOUTS:
+    AX_SIZE = (6, 4)
+    LW = 2
+    COLORS = ('b', 'g', 'r')
 
-    fig_size = (AX_SIZE[0], AX_SIZE[1] * len(fig_layout))
+    fig_size = (AX_SIZE[0] * AX_GRID[1], AX_SIZE[1] * AX_GRID[0])
 
-    fig, axs = plt.subplots(len(fig_layout), 1,
-                            facecolor=FACE_COLOR,
+    fig, axs = plt.subplots(*AX_GRID,
                             figsize=fig_size,
                             tight_layout=True)
 
-    for ax, ax_layout in zip(axs, fig_layout):
+    for ax, (expt_id, variable) in zip(axs.flatten(), cproduct(EXPERIMENTS, VARIABLES)):
 
-        for data_ctr, data_layout in enumerate(ax_layout[0]):
+        handles = []
 
-            variable, experiment_id, odor_state = data_layout
+        for odor_state, color in zip(ODOR_STATES, COLORS):
 
-            tp_dstr = session.query(models.TimepointDistribution).\
-                filter_by(variable=variable, experiment_id=experiment_id, odor_state=odor_state).first()
+            tp_dstr = session.query(models.TimepointDistribution).filter_by(
+                variable=variable, experiment_id=expt_id,
+                odor_state=odor_state).first()
 
-            ax.plot(tp_dstr.bincs, tp_dstr.cts, lw=LW, color=COLORS[data_ctr])
+            handles.append(ax.plot(
+                tp_dstr.bincs, tp_dstr.cts, lw=LW, color=color, label=odor_state)[0])
 
-        ax.set_xlabel(ax_layout[1][0])
-        ax.set_ylabel(ax_layout[1][1])
+        ax.set_xlabel(variable)
+        ax.set_ylabel('counts')
 
-        plot.set_font_size(ax, FONT_SIZE)
+        ax.legend(handles=handles)
 
-plt.show()
+        ax.set_title('{}\n{}'.format(expt_id, variable))
+
+    for ax in axs.flatten():
+
+        set_font_size(ax, 16)
+
+    return fig
