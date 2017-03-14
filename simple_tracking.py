3e3b67a8ae9521e95e0c74661da91d6148238f99
rkp8000 <rpang.contact@gmail.com>
2017-03-14 16:49:05 -0700

    Add surging model with same basic dynamics as centerline-inferring agent, except that surges upon crossing the plume.

--- simple_tracking.py
+++ simple_tracking.py
@@ -288,3 +288,178 @@ class CenterlineInferringAgent(object):
         }
 
         return return_dict
+
+
+class SurgingAgent(object):
+    """
+    Tracking agent model with same base dynamics as Centerline-inferring model
+    except that plume-crossing triggers a brief upwind surge, and there is
+    no memory across multiple crossings.
+    """
+
+    def __init__(
+            self, tau, noise, bias, threshold, hit_trigger,
+            surge_amp, tau_surge, bounds):
+
+        self.tau = tau
+        self.noise = noise
+        self.bias = bias
+        self.threshold = threshold
+        self.hit_trigger = hit_trigger
+
+        self.surge_amp = surge_amp
+        self.tau_surge = tau_surge
+
+        self.surge_amp_ = surge_amp / (tau_surge * np.exp(-1))
+
+        self.bounds = bounds
+
+    def reflect_if_out_of_bounds(self, v, x):
+        """
+        Check if a position is within the bounds
+        :param v: current velocity
+        :param x: current position
+        :return: v, x corrected if x was out of bounds
+        """
+
+        if self.bounds is None:
+
+            return v, x
+
+        else:
+
+            v_new = v.copy()
+            x_new = x.copy()
+
+            for dim in range(3):
+
+                if x[dim] < self.bounds[dim][0]:
+
+                    v_new[dim] *= -1
+                    x_new[dim] = 2 * self.bounds[dim][0] - x[dim]
+
+                elif x[dim] > self.bounds[dim][1]:
+
+                    v_new[dim] *= -1
+                    x_new[dim] = 2 * self.bounds[dim][1] - x[dim]
+
+            return v_new, x_new
+
+    def track(self, plume, start_pos, duration, dt):
+        """
+        Track a plume using the basic algorithm.
+
+        :param plume: plume object with get_odor method
+        :param threshold: odor threshold for detection
+        :param start_pos: starting position
+        :param duration: duration of simulation
+        :param dt: numerical integration time step
+        :return:
+        """
+
+        n_steps = int(duration / dt)
+        ts = np.arange(n_steps) * dt
+
+        bs = np.nan * np.zeros((n_steps, 3))
+        surges = np.zeros(n_steps)
+
+        vs = np.nan * np.zeros((n_steps, 3))
+        xs = np.nan * np.zeros((n_steps, 3))
+        odors = np.nan * np.zeros((n_steps,))
+        hits = np.nan * np.zeros((n_steps,))
+
+        last_odor = 0
+        in_puff = False
+        hit_occurred = False
+
+        for t_ctr in range(n_steps):
+
+            if t_ctr == 0:
+
+                b = np.array([0, -start_pos[1], -start_pos[2]])
+                b *= (self.bias / np.linalg.norm(b))
+
+                v = np.array([0., 0, 0])
+                x = start_pos.copy()
+
+            else:
+
+                # get driving terms
+
+                eta = np.random.normal(0, self.noise, (3,))
+                b = np.array([0, -x[1], -x[2]])
+                b *= (self.bias / np.linalg.norm(b))
+
+                # update velocity and position
+
+                v += (dt / self.tau) * (-v + eta + b - surges[t_ctr])
+                x += v * dt
+
+            v, x = self.reflect_if_out_of_bounds(v, x)
+
+            # sample odor
+
+            odor = plume.get_odor(x)
+
+            # has hit occurred?
+
+            if self.hit_trigger == 'entry':
+
+                if odor >= self.threshold and not in_puff:
+
+                    hit = 1
+                    in_puff = True
+
+                else:
+
+                    hit = 0
+
+            elif self.hit_trigger == 'peak':
+
+                hit = 0
+
+                if odor >= self.threshold:
+
+                    if odor <= last_odor and not hit_occurred:
+
+                        hit = 1
+                        hit_occurred = True
+
+                    last_odor = odor
+
+            if odor < self.threshold:
+
+                last_odor = 0
+                in_puff = False
+                hit_occurred = False
+
+            # store data for this time step
+
+            bs[t_ctr] = b
+
+            vs[t_ctr] = v
+            xs[t_ctr] = x
+
+            odors[t_ctr] = odor
+            hits[t_ctr] = hit
+
+            # add new surge force if hit has occurred
+            if hit:
+                print('hit')
+                # compute alpha function starting at current time point
+                # and add it to surges array
+                ts_ = ts[t_ctr:] - ts[t_ctr]
+                surges[t_ctr:] += (self.surge_amp_*ts_*np.exp(-ts_/self.tau_surge))
+
+        return_dict = {
+            'surges': surges,
+            'bs': bs,
+            'vs': vs,
+            'xs': xs,
+            'odors': odors,
+            'hits': hits,
+            'ts': ts,
+        }
+
+        return return_dict
+
