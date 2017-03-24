eccd57bf4fdf7711371f42dfb46fc14d1efe4ce0
rkp8000 <rpang.contact@gmail.com>
2017-03-24 13:27:53 -0700

    Checkpoint.

--- view/final_figures/__init__.py
+++ view/final_figures/__init__.py
@@ -512,12 +512,10 @@ def early_vs_late_heading_timecourse(
     """
 
     # convert times to time steps
-
     ts_before = int(round(T_BEFORE / DT))
     ts_after = int(round(T_AFTER / DT))
 
     # loop over crossing groups
-
     x_0s_dict = {}
     headings_dict = {}
     p_vals_dict = {}
@@ -525,11 +523,9 @@ def early_vs_late_heading_timecourse(
     for cg_id in CROSSING_GROUP_IDS:
 
         # get crossing group
-
         crossing_group = session.query(models.CrossingGroup).filter_by(id=cg_id).first()
 
         # get early and late crossings
-
         crossings_dict = {}
         crossings_all = session.query(models.Crossing).filter_by(crossing_group=crossing_group)
         crossings_dict['early'] = crossings_all.filter(models.Crossing.crossing_number <= MAX_CROSSINGS_EARLY)
@@ -544,47 +540,37 @@ def early_vs_late_heading_timecourse(
             headings = []
 
             # get all initial headings, initial xs, peak concentrations, and heading time-series
-
             for crossing in crossings_dict[label]:
 
                 # throw away crossings that do not meet trigger criteria
-
                 x_0 = getattr(crossing.feature_set_basic, 'position_x_{}'.format('peak'))
                 h_0 = getattr(crossing.feature_set_basic, 'heading_xyz_{}'.format('peak'))
 
                 if not (X_0_MIN <= x_0 <= X_0_MAX):
-
                     continue
 
                 if not (H_0_MIN <= h_0 <= H_0_MAX):
-
                     continue
 
                 # store x_0 (uw/dw position)
-
                 x_0s.append(x_0)
 
                 # get and store headings
-
                 temp = crossing.timepoint_field(
                     session, 'heading_xyz', -ts_before, ts_after - 1,
                     'peak', 'peak', nan_pad=True)
 
                 # subtract initial heading if desired
-
                 if SUBTRACT_INITIAL_HEADING:
-
                     temp -= temp[ts_before]
 
                 # store headings
-
                 headings.append(temp)
 
             x_0s_dict[cg_id][label] = np.array(x_0s)
             headings_dict[cg_id][label] = np.array(headings)
 
         # loop through all time points and calculate p-value (ks-test) between early and late
-
         p_vals = []
 
         for t_step in range(ts_before + ts_after):
@@ -599,21 +585,16 @@ def early_vs_late_heading_timecourse(
 
         p_vals_dict[cg_id] = p_vals
 
-
     ## MAKE PLOTS
-
     t = np.arange(-ts_before, ts_after) * DT
 
     # history-dependence
-
     fig_size = (AX_SIZE[0] * AX_GRID[1], AX_SIZE[1] * AX_GRID[0])
-
     fig_0, axs_0 = plt.subplots(*AX_GRID, figsize=fig_size, tight_layout=True)
 
     for cg_id, ax in zip(CROSSING_GROUP_IDS, axs_0.flat):
 
         # get mean and sem of headings for early and late groups
-
         handles = []
 
         for label, color in EARLY_LATE_COLORS.items():
@@ -629,27 +610,21 @@ def early_vs_late_heading_timecourse(
         ax.set_xlabel('time since crossing (s)')
 
         if SUBTRACT_INITIAL_HEADING:
-
             ax.set_ylabel('$\Delta$ heading (deg.)')
-
         else:
-
             ax.set_ylabel('heading (deg.)')
 
         ax.set_title(CROSSING_GROUP_LABELS[cg_id])
 
         if cg_id == LEGEND_CROSSING_GROUP_ID:
-
             ax.legend(handles=handles, loc='upper right')
 
         set_fontsize(ax, FONT_SIZE)
 
         # plot p-value
-
         ax_twin = ax.twinx()
 
         ax_twin.plot(t, p_vals_dict[cg_id], color=P_VAL_COLOR, lw=2, ls='--', zorder=0)
-
         ax_twin.axhline(0.05, ls='-', lw=2, color='gray')
 
         ax_twin.set_ylim(*P_VAL_Y_LIM)
@@ -658,21 +633,17 @@ def early_vs_late_heading_timecourse(
         set_fontsize(ax_twin, FONT_SIZE)
 
     # position histograms
-
     bincs = 0.5 * (X_0_BINS[:-1] + X_0_BINS[1:])
-
     fig_1, axs_1 = plt.subplots(*AX_GRID, figsize=fig_size, tight_layout=True)
 
     for cg_id, ax in zip(CROSSING_GROUP_IDS, axs_1.flat):
 
         # create early and late histograms
-
         handles = []
 
         for label, color in EARLY_LATE_COLORS.items():
 
             probs = np.histogram(x_0s_dict[cg_id][label], bins=X_0_BINS, normed=True)[0]
-
             handles.append(ax.plot(100*bincs, probs, lw=2, color=color, label=label)[0])
 
         p_val = ks_2samp(x_0s_dict[cg_id]['early'], x_0s_dict[cg_id]['late'])[1]
@@ -685,9 +656,7 @@ def early_vs_late_heading_timecourse(
             CROSSING_GROUP_LABELS[cg_id], 100 * x_0_mean_diff, p_val)
 
         ax.set_title(title)
-
         ax.legend(handles=handles, loc='best')
-
         set_fontsize(ax, FONT_SIZE)
 
     return fig_0, fig_1
