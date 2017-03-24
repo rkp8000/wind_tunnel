5e8c02c827da9f6c49e0f95f081cc6a7efa2156e
rkp8000 <rpang.contact@gmail.com>
2017-03-24 13:17:50 -0700

    Clean up a touch.

--- simple_models.py
+++ simple_models.py
@@ -18,28 +18,22 @@ class ThresholdLinearHeadingConcModel(object):
     def brute_force_fit(self, hs, c_maxs, x_0s, h_0s):
 
         # try all thresholds that will separate data into unique classes
-
-        c_maxs_sorted = np.array(sorted(c_maxs))
+        c_maxs_sorted = np.array(sorted(np.unique(c_maxs)))
         thresholds = 0.5 * (c_maxs_sorted[:-1] + c_maxs_sorted[1:])
 
         # group predictors together
-
         predictors = np.array([c_maxs, x_0s, h_0s]).T
-
         rss_best = np.inf
 
         for threshold in thresholds:
 
             # split data
-
             above_mask = c_maxs >= threshold
 
             # fit models and get predictions
-
             ## when c_max is below putative threshold
 
             lm_below = linear_model.LinearRegression()
-
             lm_below.fit(predictors[~above_mask, 1:], hs[~above_mask])
             rss_below = np.sum(
                 (hs[~above_mask] - lm_below.predict(predictors[~above_mask, 1:])) ** 2)
@@ -49,19 +43,16 @@ class ThresholdLinearHeadingConcModel(object):
             lm_above = linear_model.LinearRegression()
 
             if self.include_c_max_coefficient:
-
                 lm_above.fit(predictors[above_mask, :], hs[above_mask])
                 rss_above = np.sum(
                     (hs[above_mask] - lm_above.predict(predictors[above_mask, :])) ** 2)
 
             else:
-
                 lm_above.fit(predictors[above_mask, 1:], hs[above_mask])
                 rss_above = np.sum(
                     (hs[above_mask] - lm_above.predict(predictors[above_mask, 1:])) ** 2)
 
             if rss_below + rss_above < rss_best:
-
                 rss_best = rss_below + rss_above
 
                 self.threshold = threshold
@@ -71,11 +62,8 @@ class ThresholdLinearHeadingConcModel(object):
     def predict(self, c_maxs, x_0s, h_0s):
 
         predictors = np.array([c_maxs, x_0s, h_0s]).T
-
         above_mask = c_maxs >= self.threshold
-
         hs_predicted = np.nan * np.zeros(c_maxs.shape)
-
         hs_predicted[~above_mask] = \
             self.linear_models['below'].predict(predictors[~above_mask, 1:])
 
