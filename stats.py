9f60e5799b553b5d37122d68b125c1fe64658269
rkp8000 <rpang@uw.edu>
2017-05-21 19:02:18 -0700

    Add t-test variation where an adjusted n can be supplied.

--- stats.py
+++ stats.py
@@ -4,6 +4,7 @@ Some statistical functions.
 from __future__ import print_function, division
 import numpy as np
 from scipy import stats
+from scipy.stats import distributions
 from sklearn import linear_model
 
 
@@ -146,3 +147,56 @@ def partial_corr(x, y, controls):
         residuals.append(v - lrg.predict(control_array))
 
     return stats.pearsonr(residuals[0], residuals[1])
+
+
+def _chk2_asarray(a, b, axis):
+    if axis is None:
+        a = np.ravel(a)
+        b = np.ravel(b)
+        outaxis = 0
+    else:
+        a = np.asarray(a)
+        b = np.asarray(b)
+        outaxis = axis
+    return a, b, outaxis
+
+
+def _ttest_finish(df,t):
+    """Common code between all 3 t-test functions."""
+    prob = distributions.t.sf(np.abs(t), df) * 2  # use np.abs to get upper tail
+    if t.ndim == 0:
+        t = t[()]
+
+    return t, prob
+
+
+def ttest_adjusted_ns(a, b, n1, n2, axis=0, equal_var=True):
+    """
+    Calculate a t-test except with different n's from the number of samples.
+    """
+    a, b, axis = _chk2_asarray(a, b, axis)
+    if a.size == 0 or b.size == 0:
+        return (np.nan, np.nan)
+
+    v1 = np.var(a, axis, ddof=1)
+    v2 = np.var(b, axis, ddof=1)
+
+    if (equal_var):
+        df = n1 + n2 - 2
+        svar = ((n1 - 1) * v1 + (n2 - 1) * v2) / float(df)
+        denom = np.sqrt(svar * (1.0 / n1 + 1.0 / n2))
+    else:
+        vn1 = v1 / n1
+        vn2 = v2 / n2
+        df = ((vn1 + vn2) ** 2) / ((vn1 ** 2) / (n1 - 1) + (vn2 ** 2) / (n2 - 1))
+
+        # If df is undefined, variances are zero (assumes n1 > 0 & n2 > 0).
+        # Hence it doesn't matter what df is as long as it's not NaN.
+        df = np.where(np.isnan(df), 1, df)
+        denom = np.sqrt(vn1 + vn2)
+
+    d = np.mean(a, axis) - np.mean(b, axis)
+    t = np.divide(d, denom)
+    t, prob = _ttest_finish(df, t)
+
+    return t, prob
